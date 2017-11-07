b40eb8a23a379dc63442da87761847b234ed570f
antschreier82 <as0858525@otc.edu>
2017-11-07 10:41:56 -0600

    Update

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 var _= require('lodash');
+
+var readJson = require('read-json-file');
+
+readJSON(data.json, function(error, data){
+   if (error) {
+       throw error;
+   }
+   console.log(data);
+});
